3ff74b7bc8cfade6cb6d33ec3dec2d345357df43
Loick Cherimont <loickcherimont@gmail.com>
2025-08-06 06:52:18 +0200

    Prototype the style for contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,6 @@
 export default function Contact() {
-    return <div>
-        <h2>CONTACT</h2>
+    return <div id="contact" className="h-screen">
+        <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">CONTACT</h2>
+        <p className="scroll-m-20 text-xl font-semibold tracking-tight italic">TODO...</p>
     </div>
 }
