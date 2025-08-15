ff04fe2bd6b7a01813577c6b10e05b43622e0484
Loick Cherimont <loickcherimont@gmail.com>
2025-08-15 08:27:38 +0200

    refacto: add p tag for "All rights reserved." and center it with flexbox

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,9 @@
 export default function Footer() {
     const getCurrentYear = () => (new Date()).getFullYear();
     return (
-        <footer className="border-t mx-5 p-5 flex justify-center">
-            <p>&copy; {getCurrentYear()} Loick Cherimont | All rights reserved.</p> 
+        <footer className="border-t mx-5 p-5 flex flex-col items-center">
+            <p>&copy; {getCurrentYear()} Loick Cherimont</p>
+            <p>All rights reserved.</p>
         </footer>
     )
 }
