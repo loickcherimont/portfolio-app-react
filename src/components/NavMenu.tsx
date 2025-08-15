2b4816279654fa4b5df8060661c0f1b6f8206b95
Loick Cherimont <loickcherimont@gmail.com>
2025-08-15 08:31:26 +0200

    style: use the same style as Shadcn.ui's button for mobile menu.

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -9,17 +9,17 @@ export default function NavMenu({ menuItems }: { menuItems: string[] }) {
     return <nav className="flex flex-row-reverse">
         {/* Hamburger Button */}
         <Button
-            className="p-2 text-gray-700 focus:outline-none m-1 md:hidden"
+            className="text-slate-300 focus:outline-none m-1 md:hidden"
             onClick={() => setIsOpen(!isOpen)}
             aria-label="Toggle menu"
         >
             {isOpen ? <X size={24} /> : <Menu size={24} />}
         </Button>
         {isOpen && <NavigationMenu>
-            <NavigationMenuList className="space-x-5 flex flex-col">
+            <NavigationMenuList className="space-x-5 flex flex-col bg-slate-950 text-slate-300 rounded-md">
                 {menuItems.map((item, index) =>
                     <NavigationMenuItem key={index} >
-                        <NavigationMenuLink href={`#${item.toLowerCase()}`} className="block px-4 py-2 text-gray-800 hover:bg-slate-200 rounded" onClick={() => setIsOpen(false)}>{item.toUpperCase()}</NavigationMenuLink>
+                        <NavigationMenuLink href={`#${item.toLowerCase()}`} className="block px-4 py-2 hover:bg-slate-200 rounded" onClick={() => setIsOpen(false)}>{item.toUpperCase()}</NavigationMenuLink>
                     </NavigationMenuItem>)}
             </NavigationMenuList>
         </NavigationMenu>}
