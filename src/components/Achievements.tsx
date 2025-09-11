ba125ff80ccf3b56413d2224ccb7b9634286fbe9
Loick Cherimont <loickcherimont@gmail.com>
2025-09-11 23:40:22 +0200

    feat: inform user when back server is unreachable

--- src/components/Achievements.tsx
+++ src/components/Achievements.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react"
-import { Card, CardHeader, CardTitle, CardContent, CardFooter } from "./ui/card";
+import { Card, CardHeader, CardTitle, CardContent, CardFooter, CardDescription } from "./ui/card";
 import { Button } from "./ui/button";
 import { useNavigate } from "react-router";
 import type { Project } from "@/lib/type/Project";
+import { AlertTriangle, Loader2 } from "lucide-react";
 
 export default function Achievements() {
     const navigate = useNavigate()
@@ -18,17 +19,37 @@ export default function Achievements() {
             setProjects(projects);
 
         } catch (err: unknown) {
-
+            console.error("Error from Achivements.tsx: ", err);
         }
     }
 
     const handleSeeProject = (id: number) => {
-       navigate(`/project/${id}`);
+        navigate(`/project/${id}`);
+    }
+
+    const handleRetry = () => {
+        window.location.reload();
     }
 
     useEffect(() => {
         loadProjects();
     }, []);
+
+    if (!projects) return <div className="min-h-screen flex flex-col items-center justify-center bg-slate-50 p-4 space-y-3">
+        <h2 className="text-3xl font-semibold text-center">PROJECTS</h2>
+
+      <Card className="max-w-md w-full shadow-lg border border-slate-200">
+        <CardHeader className="flex flex-col items-center text-center space-y-2">
+          <Loader2 className="w-12 h-12 animate-spin text-blue-500" />
+          <CardTitle className="text-2xl">Loading Projects...</CardTitle>
+        </CardHeader>
+        <CardContent className="text-center">
+          <CardDescription>
+            Please wait while we fetch your projects from the server.
+          </CardDescription>
+        </CardContent>
+      </Card>
+    </div>
     return <div id="projects" className="flex flex-col items-center space-y-3 md:space-y-10 min-h-screen my-10">
         <h2 className="text-3xl font-semibold text-center">PROJECTS</h2>
         <div className="flex flex-wrap justify-center items-baseline space-y-5 md:space-x-10 md:space-y-10">
