104a1bfb321b39e82192a97ed14d079b205650ae
Loick Cherimont <loickcherimont@gmail.com>
2025-09-19 00:41:25 +0200

    refactor: change position size and comment temp the button for CV

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,28 +1,28 @@
-import { Button } from "./ui/button";
+// import { Button } from "./ui/button";
 
 
 export default function Home() {
-    const handleGetCv = () => {
-        // TODO: Store CV
-        alert("CV downloaded")
-    }
+    // const handleGetCv = () => {
+    //     // TODO: Store CV
+    //     alert("CV downloaded")
+    // }
     return <div id="home" className="min-h-screen">
         <div className="flex flex-col items-center justify-around">
             {/* PHOTO */}
             {/* Title */}
             {/* Position */}
-            <div className="flex flex-col items-center space-y-3 mt-5">
+            <div className="flex flex-col items-center gap-y-5 mt-5">
                 <img src="https://placehold.co/300" alt="Developer's photo" className="rounded-lg" />
                 <h2 className="border-b pb-2 text-3xl font-semibold">Loick CHERIMONT</h2>
-                <p className="leading-7">Spring/TypeScript Developer</p>
+                <p className="text-lg">Java Fullstack Developer</p>
             </div>
 
             {/* BUTTONS */}
             {/* Hire me */}
             {/* CV */}
-            <div className="flex flex-col space-y-5 md:flex-row md: space-x-5">
+            <div className="flex flex-col mt-5 md:flex-row">
                 <a href="#contact" className="w-32 bg-slate-200 text-center py-1.5 rounded-md transition ease-in duration-300 hover:bg-slate-300">Hire me</a>
-                <Button className="w-32 cursor-pointer" onClick={handleGetCv}>Get my CV</Button>
+                {/* <Button className="w-32 cursor-pointer" onClick={handleGetCv}>Get my CV</Button> */}
             </div>
         </div>
     </div>
