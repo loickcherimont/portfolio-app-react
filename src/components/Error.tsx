0c5ef8a03f01950d517f49f213c23cee9c50adee
Loick Cherimont <loickcherimont@gmail.com>
2025-09-27 19:15:38 +0200

    style: display for wrong url, a styled 404 error page

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -5,8 +5,13 @@ export default function Error() {
     return (
         <div className="relative min-h-screen text-slate-300">
             <StarryBackground />
-            <h1>Error 404</h1>
-            <p>This path doesn't exist, use instead <NavLink to={"/"} end>this link for root</NavLink>.</p>
+            {/* Content above */}
+            <div className="relative z-10 flex flex-col items-center justify-center min-h-screen">
+                <h1 className="text-4xl font-bold">Error 404</h1>
+                <p>
+                    This path doesn't exist, use instead <NavLink to="/portfolio" end className="text-blue-400 underline">this link for root</NavLink>.
+                </p>
+            </div>
         </div>
     )
 }
