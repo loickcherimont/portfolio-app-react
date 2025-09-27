29e2de1df45aae3dcd8b5f9e6cd86adb445e66c3
Loick Cherimont <loickcherimont@gmail.com>
2025-09-27 19:19:49 +0200

    refactor: increase font-size and use new const API_URL

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,17 +4,14 @@ import { Button } from "./ui/button";
 import { useNavigate } from "react-router";
 import type { Project } from "@/lib/type/Project";
 import { Loader2 } from "lucide-react";
+import { API_URL } from "@/constants";
 
 export default function Achievements() {
     const navigate = useNavigate()
     const [projects, setProjects] = useState<Project[]>();
     const loadProjects = async () => {
         try {
-            // PROD
-            const response = await fetch(`https://portfolio-api-v1j0.onrender.com/api/projects`);
-
-            // DEV
-            // const response = await fetch(`http://localhost:8080/api/projects`);
+            const response = await fetch(`${API_URL}/api/projects`);
 
             if (!response.ok) throw new Error("Server cannot be joined!");
 
@@ -23,7 +20,7 @@ export default function Achievements() {
             setProjects(projects);
 
         } catch (err: unknown) {
-            console.error("Error from Achivements.tsx: ", err);
+            console.error("Error from Projects.tsx: ", err);
         }
     }
 
@@ -31,44 +28,40 @@ export default function Achievements() {
         navigate(`/portfolio/projects/${id}`);
     }
 
-    // const handleRetry = () => {
-    //     window.location.reload();
-    // }
-
     useEffect(() => {
         loadProjects();
     }, []);
 
-    if (!projects) return <div className="min-h-screen flex flex-col items-center justify-center p-4 space-y-3" id="projects">
+    if (!projects) return <div className="min-h-screen flex flex-col items-center p-4 gap-y-45" id="projects">
         <h2 className="border-b p-3 text-3xl font-semibold text-center">PROJECTS</h2>
+            <Card className="max-w-md w-full shadow-lg bg-black border-0">
+                <CardHeader className="flex flex-col items-center text-center space-y-2">
+                    <Loader2 className="w-12 h-12 animate-spin text-violet-700" />
+                    <CardTitle className="text-3xl text-violet-700 animate-pulse">Loading Projects...</CardTitle>
+                </CardHeader>
+                <CardContent className="text-center">
+                    <CardDescription className="text-lg">
+                        Please wait while we fetch projects from the server.
+                    </CardDescription>
+                </CardContent>
+            </Card>
 
-      <Card className="max-w-md w-full shadow-lg bg-black border-0">
-        <CardHeader className="flex flex-col items-center text-center space-y-2">
-          <Loader2 className="w-12 h-12 animate-spin text-violet-700" />
-          <CardTitle className="text-2xl text-violet-700 animate-pulse">Loading Projects...</CardTitle>
-        </CardHeader>
-        <CardContent className="text-center">
-          <CardDescription>
-            Please wait while we fetch projects from the server.
-          </CardDescription>
-        </CardContent>
-      </Card>
     </div>
-    return <div id="projects" className="flex flex-col items-center space-y-3 md:space-y-10 min-h-screen my-10">
+    return <div id="projects" className="min-h-screen flex flex-col items-center p-4 gap-y-45">
         <h2 className="border-b p-3 text-3xl font-semibold text-center">PROJECTS</h2>
         <div className="flex flex-wrap justify-center items-baseline space-y-5 md:space-x-10 md:space-y-10">
 
             {projects?.map((project: Project) => {
                 return (
-                    <Card key={project.id} className="w-80 bg-black border-0 hover:-translate-y-2 transition ease-in duration-300">
+                    <Card key={project.id} className="w-80 bg-black/50 border-0 hover:-translate-y-2 transition ease-in duration-300 text-xl">
                         <CardHeader>
-                            <CardTitle className="text-lg font-semibold tracking-tight text-violet-700">{project.title}</CardTitle>
+                            <CardTitle className="font-semibold tracking-tight text-violet-700">{project.title}</CardTitle>
                         </CardHeader>
                         <CardContent>
                             <img src={project.imageUrl} alt={`Preview for project no.${project.id}`} className="rounded-lg" />
                         </CardContent>
                         <CardFooter>
-                            <Button className="cursor-pointer bg-violet-700 text-black hover:bg-violet-500 transition ease-in duration-300" onClick={() => handleSeeProject(project.id)}>Know more</Button>
+                            <Button className="cursor-pointer bg-violet-700 text-black hover:bg-violet-500 transition ease-in duration-300 text-lg p-5" onClick={() => handleSeeProject(project.id)}>Know more</Button>
                         </CardFooter>
                     </Card>
                 );
